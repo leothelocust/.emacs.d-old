cf85f71e85f71517dd1ecc7403fead0fd58e88a9
Chris Bergquist <chris.bergquist@thit.com>
2017-10-20 12:06:05 -0500

    Go support and bockup/autosave files moved to .emacs.d

--- init.el
+++ init.el
@@ -6,3 +6,18 @@
 (package-initialize)
 
 (org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (org-bullets docker-compose-mode dockerfile-mode try all-the-icons neotree restclient emacs-doom-themes ledger-mode xref-js2 web-mode use-package tide solaire-mode rainbow-delimiters omnisharp markdown-mode magit json-mode js2-refactor gitignore-mode doom-themes counsel-projectile company-tern color-theme-sanityinc-tomorrow base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
