f0a2b5aff22be0ec12347fc6a041d652cc18e1b6
Levi Olson <olson.levi@gmail.com>
2018-01-16 14:29:37 -0600

    removing custom-set-variables section from init

--- init.el
+++ init.el
@@ -47,17 +47,3 @@
 (load "ui")
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (multiple-cursors which-key web-mode use-package tide shrink-path rainbow-delimiters py-autopep8 material-theme magit json-mode ivy-hydra iedit flycheck gorepl-mode go-playground gitignore-mode elpy eldoc-eval ein dockerfile-mode diminish counsel-projectile counsel company-go company better-defaults anzu all-the-icons))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
